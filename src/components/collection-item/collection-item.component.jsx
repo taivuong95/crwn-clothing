714b5cc2ee2437e7e26aecda37491ca38cafc020
Tai Vuong <taivt1195@gmail.com>
2020-06-13 19:28:18 +0700

    moved our directory and shop data into its own reducer, created respectiux flow for directory/map

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
-import "./collection-item.styles.scss";
 
 import CustomButton from "../custom-button/custom-button.component";
 import { addItem } from "../../redux/cart/cart.actions";
+
+import "./collection-item.styles.scss";
+
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
+
   return (
     <div className='collection-item'>
       <div
@@ -24,7 +27,9 @@ const CollectionItem = ({ item, addItem }) => {
     </div>
   );
 };
+
 const mapDispatchToProps = (dispatch) => ({
   addItem: (item) => dispatch(addItem(item)),
 });
+
 export default connect(null, mapDispatchToProps)(CollectionItem);
